0161ed90189fffaf9ac3b8c32bc2ad4a97fff765
Nobu07t <nobu0720np@gmail.com>
2023-04-03 14:43:40 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true
-  validates :password, presence: true
+         validates :name, presence: true
+         validates :profile, presence: true
+         validates :occupation, presence: true
+         validates :position, presence: true
 
   has_many :prototypes
   has_many :comments, dependent: :destroy
